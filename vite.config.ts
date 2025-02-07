20cd392af88771f3132c0f2fe80cad0095d54503
hamid <mkhairulhamid@gmail.com>
2025-02-07 10:20:38 +0700

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/weather-app/' // Replace with your actual repo name
+  base: '/weatherapp/' // Replace with your actual repo name
 })
